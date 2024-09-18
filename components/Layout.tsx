165453c70bc6d810a129252792f7e3c267560704
vicktor349 <ayomidevictor349@gmail.com>
2024-09-18 15:29:07 +0100

    Responsivity for mx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
     return (
-        <div className='mx-52 mt-10 font-Poppins'>
+        <div className='ssssm:mx-2 ssm:mx-3 sm:mx-6 lg:mx-12 xl:mx-20 2xl:mx-48 3xl:mx-52 mt-10 font-Poppins'>
             <Navbar />
             {children}
         </div>
